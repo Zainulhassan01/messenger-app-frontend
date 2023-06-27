87573d43e7057a3f5da706c8d819fbef47792036
Dev <zainulhassan1018@gmail.com>
2023-06-27 07:11:03 -0700

    Zain: Removed unnecessary files and added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3000/api';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route exact path="/" element={<h1>Welcome to the Home Page</h1>} />
+          <Route path="/login" element={<button>Login</button>} />
+          <Route path="/register" element = {<button>Register</button>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
